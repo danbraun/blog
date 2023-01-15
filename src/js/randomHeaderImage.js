4425a898af87bc500b98d4c1e7d43491f464381c
Dan Braun <danbraun@users.noreply.github.com>
2023-01-15 15:28:00 -0600

    added srcsets - post filename dating

--- src/js/randomHeaderImage.js
+++ src/js/randomHeaderImage.js
@@ -1,5 +1,3 @@
-
-
 export function randomHeaderImage(images) {
     const randomNum = Math.floor(Math.random() * images.length);
     const baseURL = "https://res.cloudinary.com/brauntrutta/image/upload/s--L43KBrpc--/t_200_circle/blog/";
