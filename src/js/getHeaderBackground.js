db8f1dadb9bb32bf5dc9d7f33036325616ecbca4
Dan Braun <danbraun@users.noreply.github.com>
2023-10-01 20:11:36 -0500

    homepage head bg from 1st post

--- src/js/getHeaderBackground.js
+++ src/js/getHeaderBackground.js
@@ -1,11 +1,36 @@
+function writeDOM(firstImage) {
+  imageForBG = firstImage.getAttribute("src")
+  const CSSStr = `header:after{background-image: url(${imageForBG})}`
+  const styleTag = document.createElement("style")
+  styleTag.innerHTML = CSSStr
+  document.head.insertAdjacentElement("beforeend", styleTag)
+}
 export function getHeaderBackground() {
-   const firstImage = document.querySelector('figure img')
+  let firstImage = document.querySelector("figure img")
 
-   if (!firstImage) return
-   
-   const imageForBG = firstImage.getAttribute('src')
-   const CSSStr = `header:after{background-image: url(${imageForBG})}`
-   const styleTag = document.createElement("style");
-   styleTag.innerHTML = CSSStr;
-   document.head.insertAdjacentElement('beforeend', styleTag);
+  if (!firstImage) {
+    const latestPostEl = document.querySelector("ul li h3 a")
+    if (!latestPostEl) return
+
+    const latestPost = latestPostEl.getAttribute("href")
+    fetch(location.href + latestPost.replace(/\//, ""))
+      .then(function (response) {
+        // The API call was successful!
+        return response.text()
+      })
+      .then(function (html) {
+        // Convert the HTML string into a document object
+        const parser = new DOMParser()
+        const doc = parser.parseFromString(html, "text/html")
+        firstImage = doc.querySelector("figure img")
+        if (!firstImage) return
+        writeDOM(firstImage)
+      })
+      .catch(function (err) {
+        // There was an error
+        console.warn("Something went wrong.", err)
+      })
+  } else {
+    writeDOM(firstImage)
+  }
 }
